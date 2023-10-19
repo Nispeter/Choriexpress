326c91b0b89ce372aa28baeb1bbeb894d4475535
JGomez1423 <JGomez2019@udec.cl>
2023-10-19 20:55:59 -0300

    añadiendo neumonia

--- Assets/Scripts/Player/Footsteps.cs
+++ Assets/Scripts/Player/Footsteps.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     public AudioSource Footsteps_Metal_Walk;
-    public AudioSource Tos;
+    public AudioSource RandomSFX;
     public AudioClip Tos1;
     public AudioClip Tos2;
     public AudioClip Tos3;
@@ -13,26 +13,28 @@ public class Footsteps : MonoBehaviour
     public int digit;
     public int digit2;
 
+    private bool _tos = false;
     void Update(){
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A)){
             Footsteps_Metal_Walk.enabled= true;
-            // digit = Random.Range(0,101);
-            // if(digit <= 50){
-            //    digit2 = Random.Range(0,5);
-            //     if(digit2 == 1){
-            //         Tos.clip = Tos1;
-            //         Tos.Play();
-            //     }if(digit2 == 2){
-            //         Tos.clip = Tos2;
-            //         Tos.Play();
-            //     }if(digit2 == 3){
-            //         Tos.clip = Tos3;
-            //         Tos.Play();
-            //     }else{
-            //         Tos.clip = Tos4;
-            //         Tos.Play();
-            //     }
-            // }
+            digit = Random.Range(0,10001);
+            if(digit <= 2 && !_tos){
+                _tos = true; 
+               digit2 = Random.Range(0,5);
+                if(digit2 == 1){
+                    RandomSFX.clip = Tos1;
+                    RandomSFX.Play();
+                }if(digit2 == 2){
+                    RandomSFX.clip = Tos2;
+                    RandomSFX.Play();
+                }if(digit2 == 3){
+                    RandomSFX.clip = Tos3;
+                    RandomSFX.Play();
+                }else{
+                    RandomSFX.clip = Tos4;
+                    RandomSFX.Play();
+                }
+            }
         }else{
             Footsteps_Metal_Walk.enabled=false;
         }
